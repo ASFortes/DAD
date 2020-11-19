3505d5de1e4eabf17ba5ab6d6a5f18611f5a2ad2
DoContra <50984871+DoContra@users.noreply.github.com>
2020-11-19 02:01:24 +0000

    initial commit
    intial commit

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>ola</h1>
+    <!-- <router-link to="/users">Users</router-link> - -->
+   
+    <router-view></router-view>
+        
+    </div>
+</template>
+<script>
+import UserComponent from './components/user'
+export default {
+    components:{
+         'user':UserComponent
+         
+    }
+}
+</script>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Vue.js App</title>
+        <!-- Latest compiled and minified CSS & JS -->
+        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+        
+        </head>
+        <body>
+        <div class="container" id="app">
+           <h2>abd</h2>
+        </div>
+     
+     <script src="js/app.js"></script>
+    </body>
+</html>
